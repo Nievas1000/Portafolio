d60bca40a78ba5c98e2f110f7fb729ec47068cd2
Lautaro Nievas <lautynievas09@gmail.com>
2024-01-25 22:45:19 -0300

    about me changed

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,17 +10,25 @@ const SobreMi = () => {
     >
       <h1>About me</h1>
       <p>
-        My name is Lautaro Nievas and I am a Web Developer with almost 2 years
-        of experience both in Frontend development and in Backend development.
+        Experienced and results-driven Software Engineer boasting nearly three
+        years of hands-on expertise in both front-end and back-end development.
+        I am recognized for my unwavering dedication to producing top-notch
+        code, and my optimistic demeanor shines through in the collaborative
+        environments I excel in.
       </p>
       <p>
-        My goals are to participate in the creation of services and applications
-        that are useful, enjoying the process since I am passionate about
-        learning and improving in the projects that I am involved in.
+        What truly distinguishes me is my fervor for independently acquiring new
+        skills, propelling me into the captivating realm of programming. This
+        self-driven initiative not only propels my professional advancement but
+        also positions me as a resourceful contributor in fast-paced and
+        evolving work settings.
       </p>
       <p>
-        I consider myself a proactive person, a great team worker and with the
-        ability to identify and solve various problems.
+        My commitment to staying abreast of the ever-changing technology
+        landscape positions me as a valuable asset to any dynamic team. I thrive
+        in environments where continuous improvement is the norm. Explore
+        further below to discover a glimpse of my diverse projects and
+        proficiency in cutting-edge technologies.
       </p>
     </div>
   );
